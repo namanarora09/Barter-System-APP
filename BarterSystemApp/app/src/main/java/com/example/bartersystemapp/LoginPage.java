e006618800ede1324c24ffaf27dcf524681697f6
namanarora09 <namanarora0908@gmail.com>
2022-02-18 20:51:50 +0530

    commit

--- BarterSystemApp/app/src/main/java/com/example/bartersystemapp/LoginPage.java
+++ BarterSystemApp/app/src/main/java/com/example/bartersystemapp/LoginPage.java
@@ -2,7 +2,11 @@ package com.example.bartersystemapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginPage extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class LoginPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        Button signup=findViewById(R.id.signUp);
+        EditText email=findViewById(R.id.email);
+        EditText password=findViewById(R.id.password);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(LoginPage.this,SignUpPage.class);
+                startActivity(i);
+            }
+        });
     }
 }
