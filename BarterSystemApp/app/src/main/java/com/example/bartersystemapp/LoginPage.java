6ec35fff1d92fd7cf09399e5895741aa14296752
namanarora09 <namanarora0908@gmail.com>
2022-02-22 21:19:44 +0530

    Home Page

--- BarterSystemApp/app/src/main/java/com/example/bartersystemapp/LoginPage.java
+++ BarterSystemApp/app/src/main/java/com/example/bartersystemapp/LoginPage.java
@@ -15,14 +15,29 @@ public class LoginPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
         Button signup=findViewById(R.id.signUp);
+        Button signin=findViewById(R.id.signIn);
         EditText email=findViewById(R.id.email);
         EditText password=findViewById(R.id.password);
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i=new Intent(LoginPage.this,SearchResult.class);
+                Intent i=new Intent(LoginPage.this,SignUpPage.class);
                 startActivity(i);
             }
+        });
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i1=new Intent(LoginPage.this,HomePage.class);
+                 String emailId=email.getText().toString();
+                    String pass=password.getText().toString();
+                    if (emailId.equals("barter@gmail.com")) {
+                        if(pass.equals("barter")) {
+                            startActivity(i1);
+                        }
+
+                    }
+                }
         });
     }
 }
